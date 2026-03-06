74cfdd9589957304301533d46d8d2de2b4b0ae75
Mamoutou <mamoutoufofana@mac.home>
2026-03-06 13:12:49 +0100

    Add tavily search node definition

---
+++ backend/app/graph/nodes/travily_search_node.py
@@ -0,0 +1,33 @@
+from app.graph.state import HealthBotState
+from app.services.tavily_service import TavilyService
+
+
+
+class TravilySearchNode(HealthBotState):
+    """
+    LangGraph node responsible for retrieving medical information.
+
+    This node queries the Tavily search engine using the topic
+    provided by the patient and stores the results in the workflow state.
+    """
+
+    def __init__(self,):
+        self.search_service = TavilyService()
+
+    def __call__(self,state: HealthBotState) -> HealthBotState:
+        """
+        Execute the search step of the workflow.
+
+        Args:
+            state (HealthBotState): Current workflow state.
+
+        Returns:
+            HealthBotState: Updated state including search results.
+        """
+        topic = state["topic"]
+        results = self.search_service.search(topic)
+
+        return {
+            **state,
+            "results": results,
+        }
